8f9a0c296e15d2edaf598a2137ced5bf3f97426c
Narendra580718 <narendramj7@gmail.com>
2023-07-29 15:02:04 +0545

    UPDATED

---
+++ coursework.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 29, 2023 at 11:16 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coursework`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `Name` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Order` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- dataprocess.php
+++ dataprocess.php
@@ -1,30 +1,85 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "coursework";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    // Retrieve form data
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $name = $_POST["name"];
 
+    $email = $_POST["email"];
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $order = $_POST["order"];
+
+ 
+
+    // Validate data (you can add more specific validations here)
+
+    if (empty($name) || empty($email) || empty($order)) {
+
+        echo "Please fill out all the fields.";
+
+    } else {
+
+        // Database connection parameters
+
+        $dbHost = "localhost" ; // Replace with your database host (e.g., "localhost")
+
+        $dbUser = "root"; // Replace with your database username
+
+        $dbPass = ""; // Replace with your database password
+
+        $dbName = "coursework";
+
+ 
+
+        // Create a database connection
+
+        $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+ 
+
+        // Check connection
+
+        if ($conn->connect_error) {
+            die("Connection Failed: " .$conn->connect_error);
+        }
+
+ 
+
+        // Prepare and execute the SQL query to insert data into the "orders" table
+
+        $sql = "INSERT INTO orders (Name, Email, Order) VALUES (?, ?, ?)";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bind_param("sss", $name, $email, $order);
+
+        $stmt->execute();
+
+ 
+
+        // Close the database connection
+
+        $stmt->close();
+
+        $conn->close();
+
+ 
+
+        // Display a confirmation message
+
+        echo "Thank you, $name! Your order ($order) has been submitted successfully.";
+
+    }
+
+} else {
+
+    // If the form is not submitted through POST, redirect back to the form page
+
+    header("Location: contact.html");
+
+    exit();
+
 }
 
-
-$stmt = $conn->prepare('INSERT INTO order(Name, Email, Order) VALUES (?, ?, ?)');
-$stmt->bind_param("sss", $name, $email, $order);
-
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$order = $_POST['order'];
-console.log($name);
-
-$stmt->execute();
-
-$stmt->close();
-$conn->close();
 ?>
